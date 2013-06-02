334d752ee1341e657da3710eeebb3060e6b0b578
Marcel Bokhorst <marcel@bokhorst.biz>
2013-06-02 13:35:02 +0200

    Add check for Xposed framework

--- src/biz/bokhorst/xprivacy/XMain.java
+++ src/biz/bokhorst/xprivacy/XMain.java
@@ -1,11 +1,23 @@
 package biz.bokhorst.xprivacy;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.view.LayoutInflater;
@@ -32,6 +44,26 @@ public class XMain extends Activity {
 		} catch (Throwable ex) {
 			XUtil.bug(null, ex);
 		}
+
+		// Check Xposed version
+		String processVersion = getInstalledAppProcessVersion(null);
+		String bridgeVersion = getJarInstalledVersion(null);
+		if (processVersion == null || bridgeVersion == null || Integer.parseInt(processVersion) < 35) {
+			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+			alertDialog.setTitle(getString(R.string.app_name));
+			alertDialog.setMessage(getString(R.string.app_notxposed));
+			alertDialog.setIcon(R.drawable.ic_launcher);
+			alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+				}
+			});
+			alertDialog.show();
+		}
+
+		// Show Xposed version
+		TextView tvXVersion = (TextView) findViewById(R.id.tvXVersion);
+		tvXVersion.setText(String.format(getString(R.string.app_xversion), processVersion, bridgeVersion));
 
 		// Fill permission list view adapter
 		final List<String> listPermission = new ArrayList<String>(XPermissions.cPermissions.keySet());
@@ -77,5 +109,66 @@ public class XMain extends Activity {
 
 			return row;
 		}
+	}
+
+	private String getInstalledAppProcessVersion(String defaultValue) {
+		try {
+			return getAppProcessVersion(new FileInputStream("/system/bin/app_process"), defaultValue);
+		} catch (IOException e) {
+			return defaultValue;
+		}
+	}
+
+	private static Pattern PATTERN_APP_PROCESS_VERSION = Pattern.compile(".*with Xposed support \\(version (.+)\\).*");
+
+	private String getAppProcessVersion(InputStream is, String defaultValue) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String line;
+		while ((line = br.readLine()) != null) {
+			if (!line.contains("Xposed"))
+				continue;
+			Matcher m = PATTERN_APP_PROCESS_VERSION.matcher(line);
+			if (m.find()) {
+				is.close();
+				return m.group(1);
+			}
+		}
+		is.close();
+		return defaultValue;
+	}
+
+	public static String getJarInstalledVersion(String defaultValue) {
+		try {
+			if (new File("/data/xposed/XposedBridge.jar.newversion").exists())
+				return getJarVersion(new FileInputStream("/data/xposed/XposedBridge.jar.newversion"), defaultValue);
+			else
+				return getJarVersion(new FileInputStream("/data/xposed/XposedBridge.jar"), defaultValue);
+		} catch (IOException e) {
+			return defaultValue;
+		}
+	}
+
+	public static String getJarVersion(InputStream is, String defaultValue) throws IOException {
+		JarInputStream jis = new JarInputStream(is);
+		JarEntry entry;
+		try {
+			while ((entry = jis.getNextJarEntry()) != null) {
+				if (!entry.getName().equals("assets/VERSION"))
+					continue;
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(jis));
+				String version = br.readLine();
+				is.close();
+				br.close();
+				return version;
+			}
+		} finally {
+			try {
+				jis.close();
+			} catch (Exception e) {
+			}
+		}
+		return defaultValue;
 	}
 }
