73fc4c92f0969f206726091b898a41ddfc2cd1f0
Marcel Bokhorst <marcel@bokhorst.biz>
2013-07-05 09:55:19 +0200

    Report exception in before and after

--- src/biz/bokhorst/xprivacy/XPrivacy.java
+++ src/biz/bokhorst/xprivacy/XPrivacy.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import android.app.AndroidAppHelper;
+import android.content.Context;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Build;
@@ -14,6 +15,7 @@ import android.os.Process;
 import android.provider.MediaStore;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
@@ -307,6 +309,7 @@ public class XPrivacy implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 							hook.before(param);
 					} catch (Throwable ex) {
 						Util.bug(null, ex);
+						report(ex);
 						throw ex;
 					}
 				}
@@ -321,6 +324,7 @@ public class XPrivacy implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 						}
 					} catch (Throwable ex) {
 						Util.bug(null, ex);
+						report(ex);
 						throw ex;
 					}
 				}
@@ -367,6 +371,14 @@ public class XPrivacy implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 			}
 		} catch (Throwable ex) {
 			Util.bug(null, ex);
+		}
+	}
+
+	private void report(Throwable ex) {
+		Context context = AndroidAppHelper.currentApplication();
+		if (context != null) {
+			Toast toast = Toast.makeText(context, ex.toString(), Toast.LENGTH_LONG);
+			toast.show();
 		}
 	}
 }
