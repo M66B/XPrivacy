30b6bc2796583b540aaec3071dc88930cb84a36d
Marcel Bokhorst <marcel@bokhorst.biz>
2013-07-15 12:36:27 +0200

    Allow /proc/self/cmdline (Facebook)

--- src/biz/bokhorst/xprivacy/XIoBridge.java
+++ src/biz/bokhorst/xprivacy/XIoBridge.java
@@ -2,6 +2,8 @@ package biz.bokhorst.xprivacy;
 
 import java.io.FileNotFoundException;
 
+import android.util.Log;
+
 import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
 
 public class XIoBridge extends XHook {
@@ -20,15 +22,17 @@ public class XIoBridge extends XHook {
 	protected void before(MethodHookParam param) throws Throwable {
 		if (param.args.length > 0) {
 			String fileName = (String) param.args[0];
-			if (fileName != null && fileName.startsWith(mFileName)) {
+			if (fileName != null && fileName.startsWith(mFileName) && !fileName.equals("/proc/self/cmdline")) {
 				// Backward compatibility
 				if (mFileName.equals("/proc"))
 					if (PrivacyManager.getSetting(this, null, PrivacyManager.cSettingVersion, null, true) == null)
 						return;
 
 				// Check if restricted
-				if (isRestricted(param, mFileName))
+				if (isRestricted(param, mFileName)) {
+					Util.log(this, Log.INFO, "File name=" + fileName);
 					param.setResult(new FileNotFoundException());
+				}
 			}
 		}
 	}
