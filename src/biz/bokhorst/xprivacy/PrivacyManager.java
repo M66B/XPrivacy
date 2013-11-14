1e302b15a84c92ae889bed69f2e800282b134b9c
M66B <marcel@bokhorst.biz>
2013-11-14 08:59:31 +0100

    Fixed crash on empty latitude/longitude
    Fixes #755

--- src/biz/bokhorst/xprivacy/PrivacyManager.java
+++ src/biz/bokhorst/xprivacy/PrivacyManager.java
@@ -875,12 +875,12 @@ public class PrivacyManager {
 		// 1 m ~ 0,000009 degrees
 		// Christmas Island ~ -10.5 / 105.667
 
-		if (sLat == null)
+		if (sLat == null || "".equals(sLat))
 			location.setLatitude(-10.5);
 		else
 			location.setLatitude(Float.parseFloat(sLat) + (Math.random() * 2.0 - 1.0) * location.getAccuracy() * 9e-6);
 
-		if (sLon == null)
+		if (sLon == null || "".equals(sLon))
 			location.setLongitude(105.667);
 		else
 			location.setLongitude(Float.parseFloat(sLon) + (Math.random() * 2.0 - 1.0) * location.getAccuracy() * 9e-6);
