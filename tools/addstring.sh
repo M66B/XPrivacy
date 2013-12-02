b959070799fca87af53064e398a62131f89d5228
jpeg729 <jpeg729@gmail.com>
2013-12-02 21:14:31 +0100

    ToggleTask for toggle operations

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+<<<<<<< HEAD
 grep -Rl "\<string name=\"help_dangerous" . | xargs sed -i -e '/string name="msg_random_boot/a \
 \ \ \ \ <string name="msg_usage">Updating usage data</string>'
+=======
+grep -Rl "\<string name=\"help_dangerous" . | xargs sed -i -e '/string name="msg_filtering/a \
+\ \ \ \ <string name="msg_applying">Applying</string>'
+>>>>>>> ToggleTask for toggle operations
 
