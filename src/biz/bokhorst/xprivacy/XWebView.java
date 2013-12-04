ef3fd3e84baf3ed017f9acc2683df4b62e5028c7
M66B <marcel@bokhorst.biz>
2013-12-04 08:51:57 +0100

    Hook loadUrl, use WebView context
    Refs #825

--- src/biz/bokhorst/xprivacy/XWebView.java
+++ src/biz/bokhorst/xprivacy/XWebView.java
@@ -3,6 +3,7 @@ package biz.bokhorst.xprivacy;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.Context;
 import android.os.Binder;
 import android.util.Log;
 import android.webkit.WebView;
@@ -14,8 +15,8 @@ public class XWebView extends XHook {
 	private static final List<String> mWebSettings = new ArrayList<String>();
 
 	private XWebView(Methods method, String restrictionName) {
-		super(restrictionName, (method == Methods.constructor ? null : method.name()),
-				(method == Methods.constructor ? "WebView.constructor" : null));
+		super(restrictionName, (method == Methods.WebView ? null : method.name()),
+				(method == Methods.WebView ? "WebView.constructor" : null));
 		mMethod = method;
 	}
 
@@ -23,17 +24,29 @@ public class XWebView extends XHook {
 		return "android.webkit.WebView";
 	}
 
+	// @formatter:off
+
+	// public WebView(Context context)
+	// public WebView(Context context, AttributeSet attrs)
+	// public WebView(Context context, AttributeSet attrs, int defStyle)
+	// public WebView(Context context, AttributeSet attrs, int defStyle, boolean privateBrowsing)
+	// protected WebView(Context context, AttributeSet attrs, int defStyle, Map<String, Object> javaScriptInterfaces, boolean privateBrowsing)
 	// public WebSettings getSettings()
+	// public void loadUrl(String url)
+	// public void loadUrl(String url, Map<String, String> additionalHttpHeaders)
 	// frameworks/base/core/java/android/webkit/WebView.java
 	// http://developer.android.com/reference/android/webkit/WebView.html
 
+	// @formatter:on
+
 	private enum Methods {
-		constructor, getSettings
+		WebView, loadUrl, getSettings
 	};
 
 	public static List<XHook> getInstances() {
 		List<XHook> listHook = new ArrayList<XHook>();
-		listHook.add(new XWebView(Methods.constructor, PrivacyManager.cView));
+		listHook.add(new XWebView(Methods.WebView, PrivacyManager.cView));
+		listHook.add(new XWebView(Methods.loadUrl, null));
 		listHook.add(new XWebView(Methods.getSettings, null));
 		return listHook;
 	}
@@ -45,7 +58,17 @@ public class XWebView extends XHook {
 
 	@Override
 	protected void after(MethodHookParam param) throws Throwable {
-		if (mMethod == Methods.constructor) {
+		if (mMethod == Methods.WebView) {
+			if (param.args.length > 0) {
+				int uid = Binder.getCallingUid();
+				Context context = (Context) param.args[1];
+				if (getRestricted(context, uid, true)) {
+					String ua = (String) PrivacyManager.getDefacedProp(Binder.getCallingUid(), "UA");
+					WebView webView = (WebView) param.thisObject;
+					webView.getSettings().setUserAgentString(ua);
+				}
+			}
+		} else if (mMethod == Methods.loadUrl) {
 			if (isRestricted(param)) {
 				String ua = (String) PrivacyManager.getDefacedProp(Binder.getCallingUid(), "UA");
 				WebView webView = (WebView) param.thisObject;
