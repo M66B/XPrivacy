06aa0b27b87150b661f8852c0876e060b34ee1e3
M66B <marcel@bokhorst.biz>
2013-12-07 11:41:39 +0100

    Server: fix character set

--- server/xprivacy.sql
+++ server/xprivacy.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.2
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 23, 2013 at 04:06 PM
+-- Generation Time: Dec 07, 2013 at 11:40 AM
 -- Server version: 5.5.31-1~dotdeb.0
 -- PHP Version: 5.3.27-1~dotdeb.0
 
@@ -28,14 +28,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `xprivacy` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `android_id_md5` text NOT NULL,
+  `android_id_md5` text CHARACTER SET utf8 NOT NULL,
   `android_sdk` int(11) NOT NULL,
   `xprivacy_version` int(11) DEFAULT NULL,
-  `application_name` text NOT NULL,
-  `package_name` text NOT NULL,
-  `package_version` text NOT NULL,
-  `restriction` text NOT NULL,
-  `method` text NOT NULL,
+  `application_name` text CHARACTER SET utf8 NOT NULL,
+  `package_name` text CHARACTER SET utf8 NOT NULL,
+  `package_version` text CHARACTER SET utf8 NOT NULL,
+  `restriction` text CHARACTER SET utf8 NOT NULL,
+  `method` text CHARACTER SET utf8 NOT NULL,
   `restricted` bit(1) NOT NULL,
   `used` bigint(13) NOT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `xprivacy` (
   UNIQUE KEY `restriction` (`android_id_md5`(50),`android_sdk`,`package_name`(100),`package_version`(50),`restriction`(20),`method`(70)),
   KEY `package` (`package_name`(100)),
   KEY `application` (`application_name`(100),`package_name`(100))
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1720096 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1982155 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
