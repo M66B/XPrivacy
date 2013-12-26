e91dae22291df8830e32eda87560c30093d34e74
M66B <marcel@bokhorst.biz>
2013-12-26 20:59:31 +0100

    XBinder cleanup

--- src/biz/bokhorst/xprivacy/XBinder.java
+++ src/biz/bokhorst/xprivacy/XBinder.java
@@ -1,21 +1,15 @@
 package biz.bokhorst.xprivacy;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.os.IBinder;
-import android.os.Parcel;
-import android.os.Process;
-import android.os.RemoteException;
 import android.util.Log;
 
 import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
 
 public class XBinder extends XHook {
 	private Methods mMethod;
-	private static Method mObtain = null;
 
 	private XBinder(Methods method, String restrictionName) {
 		super(restrictionName, method.name(), null);
@@ -45,18 +39,9 @@ public class XBinder extends XHook {
 	};
 
 	public static List<XHook> getInstances() {
-		try {
-			// static protected final Parcel obtain(int obj)
-			// frameworks/base/core/java/android/os/Parcel.java
-			mObtain = Parcel.class.getDeclaredMethod("obtain", int.class);
-			mObtain.setAccessible(true);
-		} catch (NoSuchMethodException ex) {
-			Util.bug(null, ex);
-		}
-
 		List<XHook> listHook = new ArrayList<XHook>();
-		// listHook.add(new XBinder(Methods.execTransact, null));
-		// listHook.add(new XBinder(Methods.transact, null));
+		listHook.add(new XBinder(Methods.execTransact, null));
+		listHook.add(new XBinder(Methods.transact, null));
 		return listHook;
 	}
 
@@ -64,29 +49,10 @@ public class XBinder extends XHook {
 	protected void before(MethodHookParam param) throws Throwable {
 		if (mMethod == Methods.execTransact) {
 			// Entry point from android_util_Binder.cpp's onTransact
-			if (mObtain != null) {
-				int code = (Integer) param.args[0];
-				int dataObj = (Integer) param.args[1];
-				int replyObj = (Integer) param.args[2];
-				int flags = (Integer) param.args[3];
-				Parcel data = (Parcel) mObtain.invoke(null, dataObj);
-				Parcel reply = (Parcel) mObtain.invoke(null, replyObj);
-				IBinder binder = (IBinder) param.thisObject;
-				// boolean result = execTransact(binder, code, data, reply,
-				// flags);
-				// param.setResult(result);
-			}
 		} else if (mMethod == Methods.transact) {
-			try {
-				int code = (Integer) param.args[0];
-				Parcel data = (Parcel) param.args[1];
-				Parcel reply = (Parcel) param.args[2];
-				int flags = (Integer) param.args[3];
-				IBinder binder = (IBinder) param.thisObject;
-				Log.w("XPrivacy", "transact uid=" + Process.myUid() + " name=" + binder.getInterfaceDescriptor());
-			} catch (Throwable ex) {
-				Util.bug(this, ex);
-			}
+			int flags = (Integer) param.args[3];
+			if (flags != 0 && flags != IBinder.FLAG_ONEWAY)
+				Util.log(this, Log.WARN, "Flags=" + Integer.toHexString(flags));
 		} else
 			Util.log(this, Log.WARN, "Unknown method=" + param.method.getName());
 	}
@@ -94,53 +60,5 @@ public class XBinder extends XHook {
 	@Override
 	protected void after(MethodHookParam param) throws Throwable {
 		// Do nothing
-	}
-
-	private boolean execTransact(IBinder binder, int code, Parcel data, Parcel reply, int flags) {
-		boolean res = false;
-		try {
-			// protected boolean onTransact(int code, Parcel data, Parcel reply,
-			// int flags)
-			Method onTransact = binder.getClass().getMethod("onTransact", int.class, Parcel.class, Parcel.class,
-					int.class);
-			onTransact.setAccessible(true);
-			try {
-				res = (Boolean) onTransact.invoke(binder, code, data, reply, flags);
-			} catch (InvocationTargetException ex) {
-				Throwable target = ex.getTargetException();
-				if (target == null)
-					new RuntimeException(target);
-				else if (target.getClass().equals(RemoteException.class))
-					throw (RemoteException) target;
-				else if (target.getClass().equals(RuntimeException.class))
-					throw (RuntimeException) target;
-				else if (target.getClass().equals(OutOfMemoryError.class))
-					throw (OutOfMemoryError) target;
-				else {
-					Util.bug(this, target);
-					throw new RuntimeException(target);
-				}
-			} catch (IllegalAccessException ex) {
-				Util.bug(this, ex);
-			}
-		} catch (NoSuchMethodException ex) {
-			Util.bug(this, ex);
-		} catch (RemoteException e) {
-			reply.setDataPosition(0);
-			reply.writeException(e);
-			res = true;
-		} catch (RuntimeException e) {
-			reply.setDataPosition(0);
-			reply.writeException(e);
-			res = true;
-		} catch (OutOfMemoryError e) {
-			RuntimeException re = new RuntimeException("Out of memory", e);
-			reply.setDataPosition(0);
-			reply.writeException(re);
-			res = true;
-		}
-		reply.recycle();
-		data.recycle();
-		return res;
 	}
 }
