1be1289157d93200fa3974efe3e459fc5f585a03
M66B <marcel@bokhorst.biz>
2013-12-30 07:37:47 +0100

    Server: fix bootstrap styling for buggy browsers

--- server/index.php
+++ server/index.php
@@ -260,7 +260,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="XPrivacy">
 		<meta name="author" content="M66B">
-		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link rel="shortcut icon" href="http://www.faircode.eu/favicon.ico" type="image/x-icon" />
 		<style type="text/css">
 			body { padding-left: 5px; padding-right: 5px; }
