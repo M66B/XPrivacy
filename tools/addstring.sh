d09c9e9dc6edb42e3bc391fbab5d03d7e6229508
M66B <marcel@bokhorst.biz>
2014-01-04 13:59:20 +0100

    Add description for manage permission

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-grep -RIl "\<string name=\"app_version" . | xargs sed -i -e '/string name="app_version/a \
-\ \ \ \ <string name="app_description">The ultimate privacy manager</string>'
+grep -RIl "\<string name=\"tutorial_detailslist" . | xargs sed -i -e '/string name="tutorial_detailslist/a \
+\ \ \ \ <string name="permission_manage">Manage applications, including killing</string>'
 
 #grep -RIl "\<string name=\"msg_edit" . | xargs sed -i -e '/msg_edit/d'
 #grep -RIl "\<string name=\"settings_experimental" . | xargs sed -i -e 's/Experimental features/Experimental functions/g'
