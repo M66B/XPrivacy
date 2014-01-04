71e8a91792528e736167b88ba3c8c32e7b4129a3
M66B <marcel@bokhorst.biz>
2014-01-04 13:47:25 +0100

    Make application description translatable
    - translation in Chinese
    - translation in Dutch
    
    Closes #1060

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-grep -RIl "\<string name=\"help_used" . | xargs sed -i -e '/string name="help_used/a \
-\ \ \ \ <string name="help_used_grayed">no usage data available</string>'
+grep -RIl "\<string name=\"app_version" . | xargs sed -i -e '/string name="app_version/a \
+\ \ \ \ <string name="app_description">The ultimate privacy manager</string>'
 
 #grep -RIl "\<string name=\"msg_edit" . | xargs sed -i -e '/msg_edit/d'
 #grep -RIl "\<string name=\"settings_experimental" . | xargs sed -i -e 's/Experimental features/Experimental functions/g'
