97382261ceeae3f8e97ec2bf7f724cd0f0a74118
M66B <marcel@bokhorst.biz>
2014-01-13 22:16:43 +0100

    No upgrade process for new installs

--- src/biz/bokhorst/xprivacy/PackageChange.java
+++ src/biz/bokhorst/xprivacy/PackageChange.java
@@ -138,19 +138,20 @@ public class PackageChange extends BroadcastReceiver {
 								PrivacyManager.cSettingVersion, "0.0", false));
 
 						// Upgrade
-						if (sVersion.compareTo(new Version("0.0")) != 0)
+						if (sVersion.compareTo(new Version("0.0")) != 0) {
 							Util.log(null, Log.WARN, "Starting upgrade from version " + sVersion + " to version "
 									+ pInfo.versionName);
-						for (String restrictionName : PrivacyManager.getRestrictions())
-							for (PrivacyManager.MethodDescription md : PrivacyManager.getMethods(restrictionName))
-								if (md.getFrom() != null)
-									if (sVersion.compareTo(md.getFrom()) < 0) {
-										Util.log(null, Log.WARN, "Upgrading " + md);
-										for (ApplicationInfo aInfo : pm.getInstalledApplications(0))
-											PrivacyManager.setRestricted(null, context, aInfo.uid,
-													md.getRestrictionName(), md.getName(), false, true);
-									} else
-										Util.log(null, Log.WARN, "No upgrade needed for " + md);
+							for (String restrictionName : PrivacyManager.getRestrictions())
+								for (PrivacyManager.MethodDescription md : PrivacyManager.getMethods(restrictionName))
+									if (md.getFrom() != null)
+										if (sVersion.compareTo(md.getFrom()) < 0) {
+											Util.log(null, Log.WARN, "Upgrading " + md);
+											for (ApplicationInfo aInfo : pm.getInstalledApplications(0))
+												PrivacyManager.setRestricted(null, context, aInfo.uid,
+														md.getRestrictionName(), md.getName(), false, true);
+										} else
+											Util.log(null, Log.WARN, "No upgrade needed for " + md);
+						}
 
 						// Set new version
 						PrivacyManager.setSetting(null, context, 0, PrivacyManager.cSettingVersion, pInfo.versionName);
