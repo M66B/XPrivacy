266c01c4c6b0bebec7bae518a01160d552f1625b
M66B <marcel@bokhorst.biz>
2014-01-17 20:41:12 +0100

    Fix restriction migrate

--- src/biz/bokhorst/xprivacy/PrivacyProvider.java
+++ src/biz/bokhorst/xprivacy/PrivacyProvider.java
@@ -532,11 +532,12 @@ public class PrivacyProvider extends ContentProvider {
 			// Process applications restrictions
 			PackageManager pm = context.getPackageManager();
 			for (ApplicationInfo appInfo : pm.getInstalledApplications(PackageManager.GET_META_DATA)) {
-				File prefFile = new File(getPrefFileName(PREF_RESTRICTION, +appInfo.uid));
+				File prefFile = new File(getPrefFileName(PREF_RESTRICTION, appInfo.uid));
 				if (prefFile.exists()) {
 					Util.log(null, Log.WARN, "Migrating " + prefFile);
 
-					SharedPreferences prefs = context.getSharedPreferences(PREF_SETTINGS, Context.MODE_WORLD_READABLE);
+					SharedPreferences prefs = context.getSharedPreferences(PREF_RESTRICTION + "." + appInfo.uid,
+							Context.MODE_WORLD_READABLE);
 
 					// Process restrictions
 					for (String restrictionName : PrivacyManager.getRestrictions())
