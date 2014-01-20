8676b62896ef7e1547c501ddd07bd2984a2cf9a3
M66B <marcel@bokhorst.biz>
2014-01-20 21:54:09 +0100

    Make new strings translatable

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-grep -RIl "\<string name=\"settings_experimental" . | xargs sed -i -e '/string name="settings_experimental/a \
-\ \ \ \ <string name="settings_https">Use secure connections</string>'
+grep -RIl "\<string name=\"msg_restart" . | xargs sed -i -e '/string name="msg_restart/a \
+\ \ \ \ <string name="msg_upgrading" formatted="false">Upgrading %s</string>'
+grep -RIl "\<string name=\"msg_restart" . | xargs sed -i -e '/string name="msg_restart/a \
+\ \ \ \ <string name="msg_randomizing" formatted="false">Randomizing %s</string>'
+grep -RIl "\<string name=\"msg_restart" . | xargs sed -i -e '/string name="msg_restart/a \
+\ \ \ \ <string name="msg_migrating" formatted="false">Migrating %s</string>'
+grep -RIl "\<string name=\"msg_restart" . | xargs sed -i -e '/string name="msg_restart/a \
+\ \ \ \ <string name="msg_registered">Check your e-mail to activate your device</string>'
+grep -RIl "\<string name=\"msg_restart" . | xargs sed -i -e '/string name="msg_restart/a \
+\ \ \ \ <string name="msg_register">Please register your device</string>'
 
 #grep -RIl "\<string name=\"settings_usage" . | xargs sed -i -e '/settings_usage/d'
 #grep -RIl "\<string name=\"settings_experimental" . | xargs sed -i -e 's/Experimental features/Experimental functions/g'
