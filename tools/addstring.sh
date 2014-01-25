946edd1c9eaaa63a5379580913a42a1e65ba0881
M66B <marcel@bokhorst.biz>
2014-01-25 22:51:07 +0100

    Improved texts

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 grep -RIl "\<string name=\"settings_https" res | xargs sed -i -e '/string name="settings_https/a \
-\ \ \ \ <string name="settings_syscomponents">Restrict system components (Android)</string>'
+\ \ \ \ <string name="settings_syscomponents">Restrict system components (Android) (requires restart)</string>'
 
-#grep -RIl "\<string name=\"msg_migrated" res | xargs sed -i -e '/msg_migrated/d'
+#grep -RIl "\<string name=\"settings_syscomponents" res | xargs sed -i -e '/settings_syscomponents/d'
 #grep -RIl "\<string name=\"settings_experimental" res | xargs sed -i -e 's/Experimental features/Experimental functions/g'
