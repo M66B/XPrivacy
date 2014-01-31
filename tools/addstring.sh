08f286502596ee44fbc1f208698ef4cab66dcef2
M66B <marcel@bokhorst.biz>
2014-01-31 20:59:49 +0100

    Fix format string

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -3,4 +3,4 @@ grep -RIl "\<string name=\"menu_clear_usage" res | xargs sed -i -e '/menu_clear_
 \ \ \ \ <string name="menu_clear_db">Clear all data</string>'
 
 #grep -RIl "\<string name=\"msg_ondemand" res | xargs sed -i -e '/msg_ondemand/d'
-#grep -RIl "\<string name=\"menu_app_store" res | xargs sed -i -e 's/title_sattention">Apps requiring attention first/title_sinvert">Invert sort order/g'
+#grep -RIl "\<string name=\"menu_app_store" res | xargs sed -i -e 's/%$3s/%3$s/g'
