b07fc53f3dfb469b427ca0d34600113f5ff5e804
M66B <marcel@bokhorst.biz>
2014-02-03 12:20:59 +0100

    Removed restriction Srv.getConfiguredNetworks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ Test and beta releases will have experimental functions enabled by default.
 
 * Fixed on demand choice not sticking
 * Fixed on demand asking for same restriction
+* Removed restriction *Srv.getConfiguredNetworks*
 
 [Open issues](https://github.com/M66B/XPrivacy/issues?state=open)
 
--- src/biz/bokhorst/xprivacy/XPrivacy.java
+++ src/biz/bokhorst/xprivacy/XPrivacy.java
@@ -141,9 +141,6 @@ public class XPrivacy implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 		// Web view
 		hookAll(XWebView.getInstances(), mSecret);
 
-		// Wi-Fi configuration storage
-		hookAll(XWifiConfigStore.getInstances(), mSecret);
-
 		// Intent receive
 		hookAll(XActivityThread.getInstances(), mSecret);
 
