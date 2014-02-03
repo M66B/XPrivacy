d23af3afdeb5977e63becd5df6da65bb1d923455
M66B <marcel@bokhorst.biz>
2014-02-03 17:04:58 +0100

    Less logging

--- src/biz/bokhorst/xprivacy/PrivacyService.java
+++ src/biz/bokhorst/xprivacy/PrivacyService.java
@@ -116,7 +116,6 @@ public class PrivacyService {
 				final Class<?> cam = Class.forName("com.android.server.am.ActivityManagerService");
 
 				for (Method anr : cam.getDeclaredMethods()) {
-					Util.log(null, Log.WARN, anr.toString());
 					// Foreground
 					// @formatter:off
 					// public long inputDispatchingTimedOut(int pid, final boolean aboveSystem, String reason)
