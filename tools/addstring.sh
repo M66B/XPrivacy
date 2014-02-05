4c3db5bbe25527793694034d9a5a41b9b9112af8
M66B <marcel@bokhorst.biz>
2014-02-05 10:26:42 +0100

    Add long press to help

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-grep -RIl "\<string name=\"help_shared" res | xargs sed -i -e '/help_shared/a \
-\ \ \ \ <string name="tutorial_mainheader">Select the category to restrict</string>'
+grep -RIl "\<string name=\"settings_usagedata" res | xargs sed -i -e '/settings_usagedata/a \
+\ \ \ \ <string name="help_longpress">Long press name to select</string>'
 
 #grep -RIl "\<string name=\"tutorial_mainheader" res | xargs sed -i -e '/tutorial_mainheader/d'
 #grep -RIl "\<string name=\"menu_app_store" res | xargs sed -i -e 's/%$3s/%3$s/g'
