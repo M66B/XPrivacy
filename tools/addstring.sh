b6ad75be8d104af136f3f1283a7d512612862951
M66B <marcel@bokhorst.biz>
2014-02-05 10:21:37 +0100

    Update tutorial

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-grep -RIl "\<string name=\"menu_sort" res | xargs sed -i -e '/menu_sort/a \
-\ \ \ \ <string name="menu_filter">Filter</string>'
+grep -RIl "\<string name=\"help_shared" res | xargs sed -i -e '/help_shared/a \
+\ \ \ \ <string name="tutorial_mainheader">Select the category to restrict</string>'
 
-#grep -RIl "\<string name=\"menu_clear_ondemand" res | xargs sed -i -e '/menu_clear_ondemand/d'
+#grep -RIl "\<string name=\"tutorial_mainheader" res | xargs sed -i -e '/tutorial_mainheader/d'
 #grep -RIl "\<string name=\"menu_app_store" res | xargs sed -i -e 's/%$3s/%3$s/g'
