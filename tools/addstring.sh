d5220f60ba8b150db8c4a22bff38afba425b809f
M66B <marcel@bokhorst.biz>
2014-02-08 14:15:59 +0100

    Ask nicely for e-mail address

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-grep -RIl "\<string name=\"title_sinvert" res | xargs sed -i -e '/title_sinvert/a \
-\ \ \ \ <string name="title_attempt">Usage attempt of:</string>'
+grep -RIl "\<string name=\"msg_register\"" res | xargs sed -i -e '/msg_register"/a \
+\ \ \ \ <string name="msg_email">Please enter your e-mail address:</string>'
 
 #grep -RIl "\<string name=\"msg_ondemand" res | xargs sed -i -e '/msg_ondemand/d'
 #grep -RIl "\<string name=\"menu_app_store" res | xargs sed -i -e 's/%$3s/%3$s/g'
