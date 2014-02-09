2264146913ebf9fc134d9c6c45c1fabdc46fe595
M66B <marcel@bokhorst.biz>
2014-02-09 16:55:08 +0100

    Add KitKat sdcard permissions

--- src/biz/bokhorst/xprivacy/XProcess.java
+++ src/biz/bokhorst/xprivacy/XProcess.java
@@ -51,6 +51,11 @@ public class XProcess extends XHook {
 	final static int sdcard_r = 1028; // 4.1+
 	final static int sdcard_rw = 1015; // 4.0+
 	final static int media_rw = 1023; // 4.0+
+
+	final static int sdcard_pics = 1033; // 4.4+ photos
+	final static int sdcard_av = 1034; // 4.4+ audio/video
+	final static int sdcard_all = 1035; // 4.4+ all users
+
 	final static int inet = 3003; // 4.0+
 	final static int inet_raw = 3004; // 4.0+
 
@@ -77,16 +82,20 @@ public class XProcess extends XHook {
 								listGids.add(gids[i]);
 							else
 								Util.log(this, Log.INFO, "Revoking media_rw uid=" + uid);
-						else if (gids[i] == sdcard_r || gids[i] == sdcard_rw)
+
+						else if (gids[i] == sdcard_r || gids[i] == sdcard_rw || gids[i] == sdcard_pics
+								|| gids[i] == sdcard_av || gids[i] == sdcard_all)
 							if (!(mRestrictionName.equals(PrivacyManager.cStorage) && mAction.equals("sdcard")))
 								listGids.add(gids[i]);
 							else
 								Util.log(this, Log.INFO, "Revoking sdcard_rw uid=" + uid);
+
 						else if (gids[i] == inet || gids[i] == inet_raw)
 							if (!(mRestrictionName.equals(PrivacyManager.cInternet)))
 								listGids.add(gids[i]);
 							else
 								Util.log(this, Log.INFO, "Revoking inet_raw uid=" + uid);
+
 						else
 							listGids.add(gids[i]);
 					}
