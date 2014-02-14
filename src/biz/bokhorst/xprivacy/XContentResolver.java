a14272e6026760ce8f47568f878aa9d73f3ad7e1
M66B <marcel@bokhorst.biz>
2014-02-14 15:20:52 +0100

    Simplify content resolver

--- src/biz/bokhorst/xprivacy/XContentResolver.java
+++ src/biz/bokhorst/xprivacy/XContentResolver.java
@@ -67,8 +67,8 @@ public class XContentResolver extends XHook {
 		listHook.add(new XContentResolver(Methods.getCurrentSync, PrivacyManager.cAccounts));
 		listHook.add(new XContentResolver(Methods.getCurrentSyncs, PrivacyManager.cAccounts));
 		listHook.add(new XContentResolver(Methods.getSyncAdapterTypes, PrivacyManager.cAccounts));
-		listHook.add(new XContentResolver(Methods.query, null, 1));
-		listHook.add(new XContentResolver(Methods.cquery, null, 1));
+		//listHook.add(new XContentResolver(Methods.query, null, 1));
+		//listHook.add(new XContentResolver(Methods.cquery, null, 1));
 		return listHook;
 	}
 
@@ -112,23 +112,26 @@ public class XContentResolver extends XHook {
 		// Check URI
 		if (param.args.length > 1 && param.args[0] instanceof Uri) {
 			String uri = ((Uri) param.args[0]).toString().toLowerCase();
+			Util.log(this, Log.WARN, "Before uri=" + uri + " uid=" + Binder.getCallingUid());
 			String[] projection = (param.args[1] instanceof String[] ? (String[]) param.args[1] : null);
+
 			if (uri.startsWith("content://com.android.contacts/contacts")
 					|| uri.startsWith("content://com.android.contacts/data")
 					|| uri.startsWith("content://com.android.contacts/phone_lookup")
 					|| uri.startsWith("content://com.android.contacts/raw_contacts")) {
 				String[] components = uri.replace("content://com.android.", "").split("/");
 				String methodName = components[0] + "/" + components[1].split("\\?")[0];
+
 				if (isRestrictedExtra(param, PrivacyManager.cContacts, methodName, uri)) {
 					// Modify projection
 					boolean added = false;
 					if (projection != null) {
 						List<String> listProjection = new ArrayList<String>();
 						listProjection.addAll(Arrays.asList(projection));
-						ContactID id = getIdForUri(uri);
-						if (id != null && !listProjection.contains(id.name)) {
+						ContactID cid = getIdForUri(uri);
+						if (cid != null && !listProjection.contains(cid.name)) {
 							added = true;
-							listProjection.add(id.name);
+							listProjection.add(cid.name);
 						}
 						param.args[1] = listProjection.toArray(new String[0]);
 					}
@@ -141,160 +144,166 @@ public class XContentResolver extends XHook {
 	@SuppressLint("DefaultLocale")
 	private void handleUriAfter(MethodHookParam param) throws Throwable {
 		// Check URI
-		if (param.args.length > 1 && param.args[0] instanceof Uri) {
+		if (param.args.length > 1 && param.args[0] instanceof Uri && param.getResult() != null) {
 			String uri = ((Uri) param.args[0]).toString().toLowerCase();
+			Util.log(this, Log.WARN, "After uri=" + uri + " uid=" + Binder.getCallingUid());
 			Cursor cursor = (Cursor) param.getResult();
-			if (cursor != null)
-				if (uri.startsWith("content://applications")) {
-					// Applications provider: allow selected applications
-					if (isRestrictedExtra(param, PrivacyManager.cSystem, "ApplicationsProvider", uri)) {
-						MatrixCursor result = new MatrixCursor(cursor.getColumnNames());
-						while (cursor.moveToNext()) {
-							int colPackage = cursor.getColumnIndex("package");
-							String packageName = (colPackage < 0 ? null : cursor.getString(colPackage));
-							if (packageName != null && XPackageManager.isPackageAllowed(packageName))
-								copyColumns(cursor, result);
+
+			if (uri.startsWith("content://applications")) {
+				// Applications provider: allow selected applications
+				if (isRestrictedExtra(param, PrivacyManager.cSystem, "ApplicationsProvider", uri)) {
+					MatrixCursor result = new MatrixCursor(cursor.getColumnNames());
+					while (cursor.moveToNext()) {
+						int colPackage = cursor.getColumnIndex("package");
+						String packageName = (colPackage < 0 ? null : cursor.getString(colPackage));
+						if (packageName != null && XPackageManager.isPackageAllowed(packageName))
+							copyColumns(cursor, result);
+					}
+					result.respond(cursor.getExtras());
+					param.setResult(result);
+					cursor.close();
+				}
+
+			} else if (uri.startsWith("content://com.google.android.gsf.gservices")) {
+				// Google services provider: block only android_id
+				if (param.args.length > 3 && param.args[3] != null) {
+					List<String> selectionArgs = Arrays.asList((String[]) param.args[3]);
+					if (Util.containsIgnoreCase(selectionArgs, "android_id"))
+						if (isRestrictedExtra(param, PrivacyManager.cIdentification, "GservicesProvider", uri)) {
+							MatrixCursor gsfCursor = new MatrixCursor(cursor.getColumnNames());
+							gsfCursor.addRow(new Object[] { "android_id",
+									PrivacyManager.getDefacedProp(Binder.getCallingUid(), "GSF_ID") });
+							gsfCursor.respond(cursor.getExtras());
+							param.setResult(gsfCursor);
+							cursor.close();
 						}
-						result.respond(cursor.getExtras());
-						param.setResult(result);
-						cursor.close();
-					}
+				}
 
-				} else if (uri.startsWith("content://com.google.android.gsf.gservices")) {
-					// Google services provider: block only android_id
-					if (param.args.length > 3 && param.args[3] != null) {
-						List<String> selectionArgs = Arrays.asList((String[]) param.args[3]);
-						if (Util.containsIgnoreCase(selectionArgs, "android_id"))
-							if (isRestrictedExtra(param, PrivacyManager.cIdentification, "GservicesProvider", uri)) {
-								MatrixCursor gsfCursor = new MatrixCursor(cursor.getColumnNames());
-								gsfCursor.addRow(new Object[] { "android_id",
-										PrivacyManager.getDefacedProp(Binder.getCallingUid(), "GSF_ID") });
-								gsfCursor.respond(cursor.getExtras());
-								param.setResult(gsfCursor);
-								cursor.close();
-							}
-					}
+			} else if (uri.startsWith("content://com.android.contacts/contacts")
+					|| uri.startsWith("content://com.android.contacts/data")
+					|| uri.startsWith("content://com.android.contacts/phone_lookup")
+					|| uri.startsWith("content://com.android.contacts/raw_contacts")) {
+				// Contacts provider: allow selected contacts
+				String[] components = uri.replace("content://com.android.", "").split("/");
+				String methodName = components[0] + "/" + components[1].split("\\?")[0];
+				if (isRestrictedExtra(param, PrivacyManager.cContacts, methodName, uri)) {
+					// Modify column names back
+					Object column_added = param.getObjectExtra("column_added");
+					boolean added = (column_added == null ? false : (Boolean) param.getObjectExtra("column_added"));
 
-				} else if (uri.startsWith("content://com.android.contacts/contacts")
-						|| uri.startsWith("content://com.android.contacts/data")
-						|| uri.startsWith("content://com.android.contacts/phone_lookup")
-						|| uri.startsWith("content://com.android.contacts/raw_contacts")) {
-					// Contacts provider: allow selected contacts
-					String[] components = uri.replace("content://com.android.", "").split("/");
-					String methodName = components[0] + "/" + components[1].split("\\?")[0];
-					if (isRestrictedExtra(param, PrivacyManager.cContacts, methodName, uri)) {
-						// Modify column names back
-						boolean added = (Boolean) param.getObjectExtra("column_added");
-						List<String> listColumn = new ArrayList<String>();
-						listColumn.addAll(Arrays.asList(cursor.getColumnNames()));
-						if (added)
-							listColumn.remove(listColumn.size() - 1);
+					List<String> listColumn = new ArrayList<String>();
+					listColumn.addAll(Arrays.asList(cursor.getColumnNames()));
+					if (added)
+						listColumn.remove(listColumn.size() - 1);
 
-						MatrixCursor result = new MatrixCursor(listColumn.toArray(new String[0]));
+					MatrixCursor result = new MatrixCursor(listColumn.toArray(new String[0]));
 
-						// Filter rows
-						ContactID cid = getIdForUri(uri);
-						int iid = (cid == null ? -1 : cursor.getColumnIndex(cid.name));
-						if (iid >= 0)
-							while (cursor.moveToNext()) {
-								// Check if allowed
-								long id = cursor.getLong(iid);
-								String settingName = (cid.raw ? PrivacyManager.cSettingRawContact
-										: PrivacyManager.cSettingContact);
-								boolean allowed = PrivacyManager.getSettingBool(Binder.getCallingUid(), settingName
-										+ id, false, true);
-								if (allowed)
-									copyColumns(cursor, result, listColumn.size());
-							}
-						else
-							Util.log(this, Log.ERROR, "ID missing uri=" + uri);
+					// Filter rows
+					ContactID cid = getIdForUri(uri);
+					int iid = (cid == null ? -1 : cursor.getColumnIndex(cid.name));
+					if (iid >= 0)
+						while (cursor.moveToNext()) {
+							// Check if allowed
+							long id = cursor.getLong(iid);
+							String settingName = (cid.raw ? PrivacyManager.cSettingRawContact
+									: PrivacyManager.cSettingContact);
+							boolean allowed = PrivacyManager.getSettingBool(Binder.getCallingUid(), settingName + id,
+									false, true);
+							if (allowed)
+								copyColumns(cursor, result, listColumn.size());
+						}
+					else
+						Util.log(this, Log.ERROR, "ID missing uri=" + uri);
 
-						result.respond(cursor.getExtras());
-						param.setResult(result);
-						cursor.close();
-					}
+					result.respond(cursor.getExtras());
+					param.setResult(result);
+					cursor.close();
+				}
 
-				} else {
-					// Other uri restrictions
-					String restrictionName = null;
-					String methodName = null;
-					if (uri.startsWith("content://browser")) {
-						restrictionName = PrivacyManager.cBrowser;
-						methodName = "BrowserProvider2";
-					}
+			} else {
+				// Other uri restrictions
+				String restrictionName = null;
+				String methodName = null;
+				if (uri.startsWith("content://browser")) {
+					restrictionName = PrivacyManager.cBrowser;
+					methodName = "BrowserProvider2";
+				}
 
-					else if (uri.startsWith("content://com.android.calendar")) {
-						restrictionName = PrivacyManager.cCalendar;
-						methodName = "CalendarProvider2";
-					}
+				else if (uri.startsWith("content://com.android.calendar")) {
+					restrictionName = PrivacyManager.cCalendar;
+					methodName = "CalendarProvider2";
+				}
 
-					else if (uri.startsWith("content://call_log")) {
-						restrictionName = PrivacyManager.cPhone;
-						methodName = "CallLogProvider";
-					}
+				else if (uri.startsWith("content://call_log")) {
+					restrictionName = PrivacyManager.cPhone;
+					methodName = "CallLogProvider";
+				}
 
-					else if (uri.startsWith("content://com.android.contacts/profile")) {
-						restrictionName = PrivacyManager.cContacts;
-						methodName = "contacts/profile";
-					}
+				else if (uri.startsWith("content://com.android.contacts/profile")) {
+					restrictionName = PrivacyManager.cContacts;
+					methodName = "contacts/profile";
+				}
 
-					else if (uri.startsWith("content://com.android.contacts")) {
-						restrictionName = PrivacyManager.cContacts;
-						methodName = "ContactsProvider2"; // fall-back
-					}
+				else if (uri.startsWith("content://com.android.contacts")) {
+					restrictionName = PrivacyManager.cContacts;
+					methodName = "ContactsProvider2"; // fall-back
+				}
 
-					else if (uri.startsWith("content://downloads")) {
-						restrictionName = PrivacyManager.cBrowser;
-						methodName = "Downloads";
-					}
+				else if (uri.startsWith("content://downloads")) {
+					restrictionName = PrivacyManager.cBrowser;
+					methodName = "Downloads";
+				}
 
-					else if (uri.startsWith("content://com.android.email.provider")) {
-						restrictionName = PrivacyManager.cEMail;
-						methodName = "EMailProvider";
-					}
+				else if (uri.startsWith("content://com.android.email.provider")) {
+					restrictionName = PrivacyManager.cEMail;
+					methodName = "EMailProvider";
+				}
 
-					else if (uri.startsWith("content://com.google.android.gm")) {
-						restrictionName = PrivacyManager.cEMail;
-						methodName = "GMailProvider";
-					}
+				else if (uri.startsWith("content://com.google.android.gm")) {
+					restrictionName = PrivacyManager.cEMail;
+					methodName = "GMailProvider";
+				}
 
-					else if (uri.startsWith("content://icc")) {
-						restrictionName = PrivacyManager.cContacts;
-						methodName = "IccProvider";
-					}
+				else if (uri.startsWith("content://icc")) {
+					restrictionName = PrivacyManager.cContacts;
+					methodName = "IccProvider";
+				}
 
-					else if (uri.startsWith("content://mms")) {
-						restrictionName = PrivacyManager.cMessages;
-						methodName = "MmsProvider";
-					}
+				else if (uri.startsWith("content://mms")) {
+					restrictionName = PrivacyManager.cMessages;
+					methodName = "MmsProvider";
+				}
 
-					else if (uri.startsWith("content://mms-sms")) {
-						restrictionName = PrivacyManager.cMessages;
-						methodName = "MmsSmsProvider";
-					}
+				else if (uri.startsWith("content://mms-sms")) {
+					restrictionName = PrivacyManager.cMessages;
+					methodName = "MmsSmsProvider";
+				}
 
-					else if (uri.startsWith("content://sms")) {
-						restrictionName = PrivacyManager.cMessages;
-						methodName = "SmsProvider";
-					}
+				else if (uri.startsWith("content://sms")) {
+					restrictionName = PrivacyManager.cMessages;
+					methodName = "SmsProvider";
+				}
 
-					else if (uri.startsWith("content://telephony")) {
-						restrictionName = PrivacyManager.cPhone;
-						methodName = "TelephonyProvider";
-					}
+				else if (uri.startsWith("content://telephony")) {
+					restrictionName = PrivacyManager.cPhone;
+					methodName = "TelephonyProvider";
+				}
 
-					else if (uri.startsWith("content://user_dictionary")) {
-						restrictionName = PrivacyManager.cDictionary;
-						methodName = "UserDictionary";
-					}
+				else if (uri.startsWith("content://user_dictionary")) {
+					restrictionName = PrivacyManager.cDictionary;
+					methodName = "UserDictionary";
+				}
 
-					else if (uri.startsWith("content://com.android.voicemail")) {
-						restrictionName = PrivacyManager.cMessages;
-						methodName = "VoicemailContentProvider";
-					}
+				else if (uri.startsWith("content://com.android.voicemail")) {
+					restrictionName = PrivacyManager.cMessages;
+					methodName = "VoicemailContentProvider";
+				}
 
-					// Check if know / restricted
-					if (methodName != null && isRestrictedExtra(param, restrictionName, methodName, uri)) {
+				// Check if know / restricted
+				if (restrictionName != null && methodName != null) {
+					Util.log(this, Log.WARN, "restriction=" + restrictionName + "/" + methodName + " uri=" + uri
+							+ " uid=" + Binder.getCallingUid());
+					if (isRestrictedExtra(param, restrictionName, methodName, uri)) {
 						// Return empty cursor
 						MatrixCursor result = new MatrixCursor(cursor.getColumnNames());
 						result.respond(cursor.getExtras());
@@ -302,6 +311,7 @@ public class XContentResolver extends XHook {
 						cursor.close();
 					}
 				}
+			}
 		}
 	}
 
