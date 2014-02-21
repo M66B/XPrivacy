675c7d326cdb0299a9a13b6ca00c5b52f712c3de
M66B <marcel@bokhorst.biz>
2014-02-21 14:56:32 +0100

    Remove unused text

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -2,5 +2,5 @@
 #grep -RIl "\<string name=\"msg_no_restrictions" res | xargs sed -i -e "/msg_no_restrictions/a \
 #\ \ \ \ <string name=\"msg_no_whitelists\">No whitelists found for this application</string>"
 
-grep -RIl "\<string name=\"msg_no_whitelists" res | xargs sed -i -e '/msg_no_whitelists/d'
+grep -RIl "\<string name=\"title_categories" res | xargs sed -i -e '/title_categories/d'
 #grep -RIl "\<string name=\"title_whitelist" res | xargs sed -i -e 's/">Whitelist /">Remember /g'
