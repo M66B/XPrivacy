beec535ba03ff8de2075080a375ce7fe17b1e538
M66B <marcel@bokhorst.biz>
2014-03-08 15:51:52 +0100

    Fix warning

--- src/biz/bokhorst/xprivacy/ActivityMain.java
+++ src/biz/bokhorst/xprivacy/ActivityMain.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.TimeZone;
 import java.util.TreeMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
