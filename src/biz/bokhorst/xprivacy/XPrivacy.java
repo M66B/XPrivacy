8a0ab57c4c6a4fd2dc42d8bbf3d0d7755063fb24
M66B <marcel@bokhorst.biz>
2014-03-12 08:03:18 +0100

    Catch NoSuchFieldError (support info)

--- src/biz/bokhorst/xprivacy/XPrivacy.java
+++ src/biz/bokhorst/xprivacy/XPrivacy.java
@@ -390,6 +390,8 @@ public class XPrivacy implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 			for (Member member : listMember)
 				try {
 					XposedBridge.hookMethod(member, methodHook);
+				} catch (NoSuchFieldError ex) {
+					Util.bug(hook, ex);
 				} catch (Throwable ex) {
 					mListHookError.add(ex.toString());
 					Util.bug(hook, ex);
