40652d43acacef350c77e8a22f6fa2aca6b04c21
M66B <marcel@bokhorst.biz>
2014-06-05 14:14:09 +0200

    Default for dangerous methods was asked
    Refs #1704

--- src/biz/bokhorst/xprivacy/UpdateService.java
+++ src/biz/bokhorst/xprivacy/UpdateService.java
@@ -308,6 +308,7 @@ public class UpdateService extends Service {
 					if (PrivacyManager.isRestrictionSet(restriction))
 						Util.log(null, Log.WARN, "Restrict dangerous set restriction=" + restriction);
 					else {
+						restriction.asked = true;
 						Util.log(null, Log.WARN, "Restrict dangerous setting restriction=" + restriction);
 						listWork.add(restriction);
 					}
