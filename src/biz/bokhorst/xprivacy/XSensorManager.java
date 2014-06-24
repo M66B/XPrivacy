332a7957896fc4c9c3df278a2470ac4c4638fe6c
Marcel Bokhorst <marcel@faircode.eu>
2014-06-24 15:07:53 +0200

    Added legacy temperature sensor type

--- src/biz/bokhorst/xprivacy/XSensorManager.java
+++ src/biz/bokhorst/xprivacy/XSensorManager.java
@@ -114,14 +114,14 @@ public class XSensorManager extends XHook {
 				|| type == Sensor.TYPE_ROTATION_VECTOR) {
 			if (isRestricted(param, "rotation"))
 				return true;
-		} else if (type == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+		} else if (type == Sensor.TYPE_TEMPERATURE || type == Sensor.TYPE_AMBIENT_TEMPERATURE) {
 			if (isRestricted(param, "temperature"))
 				return true;
 		} else if (type == Sensor.TYPE_STEP_COUNTER || type == Sensor.TYPE_STEP_DETECTOR) {
 			if (isRestricted(param, "step"))
 				return true;
 		} else
-			Util.log(this, Log.WARN, "Unknown sensor type=" + type);
+			Util.log(this, Log.ERROR, "Unknown sensor type=" + type);
 		return false;
 	}
 }
