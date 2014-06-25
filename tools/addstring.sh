1987437be6569b4c26551f4d5a37386ab2ec5d24
Marcel Bokhorst <marcel@faircode.eu>
2014-06-25 08:37:44 +0200

    Added hint to template

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-grep -RIl "\<string name=\"whitelist_library" res | xargs sed -i -e "/whitelist_library/a \
-\ \ \ \ <string name=\"whitelist_method\">Method names</string>"
+grep -RIl "\<string name=\"title_usage_footer" res | xargs sed -i -e "/title_usage_footer/a \
+\ \ \ \ <string name=\"title_toggle_dangerous\">Long press on a title to toggle dangerous functions</string>"
 
 #grep -RIl "\<string name=\"settings_dangerous" res | xargs sed -i -e '/settings_dangerous/d'
 #grep -RIl "\<string name=\"title_check_whitelist" res | xargs sed -i -e 's/Check to allow/Check to allow:/g'
