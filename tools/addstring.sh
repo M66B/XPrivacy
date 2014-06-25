3c9dee450d0317d71dd88b3a3f67e13b4f43f7bf
Marcel Bokhorst <marcel@faircode.eu>
2014-06-25 08:48:01 +0200

    Added remark to on demand dialog

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-grep -RIl "\<string name=\"title_usage_footer" res | xargs sed -i -e "/title_usage_footer/a \
-\ \ \ \ <string name=\"title_toggle_dangerous\">Long press on a title to toggle dangerous functions</string>"
+grep -RIl "\<string name=\"title_ondemand_category" res | xargs sed -i -e '/title_ondemand_category/d'
+grep -RIl "\<string name=\"title_toggle_dangerous" res | xargs sed -i -e "/title_toggle_dangerous/a \
+\ \ \ \ <string name=\"title_ondemand_category\">Applying to the category will reset the function exceptions to default values</string>"
 
 #grep -RIl "\<string name=\"settings_dangerous" res | xargs sed -i -e '/settings_dangerous/d'
 #grep -RIl "\<string name=\"title_check_whitelist" res | xargs sed -i -e 's/Check to allow/Check to allow:/g'
