936e88623e8c595296f87bdea73c33cd0b626c63
M66B <marcel@faircode.eu>
2014-06-26 16:48:53 +0200

    IPC exception (support info)

--- src/biz/bokhorst/xprivacy/XBinder.java
+++ src/biz/bokhorst/xprivacy/XBinder.java
@@ -148,6 +148,7 @@ public class XBinder extends XHook {
 			"android.content.BroadcastReceiver$PendingResult",
 			"android.content.ContentResolver",
 			"android.hardware.SensorManager",
+			"com.android.internal.os.RuntimeInit",
 			"android.os.StrictMode$AndroidBlockGuardPolicy",
 			"com.android.internal.app.ResolverActivity",
 			"com.android.internal.os.RuntimeInit$UncaughtHandler",
