db08cc6c21553f6a9eb7e655b15b3486d8a8b03e
M66B <marcel@faircode.eu>
2014-06-27 20:23:15 +0200

    Reduce debug logging

--- src/biz/bokhorst/xprivacy/XActivityThread.java
+++ src/biz/bokhorst/xprivacy/XActivityThread.java
@@ -11,7 +11,6 @@ import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Binder;
 import android.os.Bundle;
-import android.os.Process;
 import android.provider.Telephony;
 import android.service.notification.NotificationListenerService;
 import android.telephony.TelephonyManager;
@@ -129,8 +128,6 @@ public class XActivityThread extends XHook {
 				if (intent != null) {
 					// Check action
 					String action = intent.getAction();
-					if (action != null)
-						Util.log(this, Log.INFO, "Intent action=" + action + " uid=" + Process.myUid());
 					if (mActionName.equals(action)) {
 						if (action.equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
 							// Outgoing call
