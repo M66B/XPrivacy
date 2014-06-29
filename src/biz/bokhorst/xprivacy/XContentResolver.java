890e0d59f42c802e4b1dd9bced397a3c67df27cc
M66B <marcel@faircode.eu>
2014-06-29 14:09:10 +0200

    Fix throwable

--- src/biz/bokhorst/xprivacy/XContentResolver.java
+++ src/biz/bokhorst/xprivacy/XContentResolver.java
@@ -136,7 +136,7 @@ public class XContentResolver extends XHook {
 			if (param.args.length > 0 && param.args[0] instanceof Uri) {
 				String uri = ((Uri) param.args[0]).toString();
 				if (isRestrictedExtra(param, uri))
-					param.setResult(new FileNotFoundException("XPrivacy"));
+					param.setThrowable(new FileNotFoundException("XPrivacy"));
 			}
 
 		} else if (mMethod == Methods.query) {
