c45a6bb46514874031c924d1e02351db116c9d66
M66B <marcel@faircode.eu>
2014-07-06 10:53:40 +0200

    Better title for on demand default

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-grep -RIl "\<string name=\"title_function_help" res | xargs sed -i -e '/title_function_help/d'
-grep -RIl "\<string name=\"title_ondemand_category" res | xargs sed -i -e "/title_ondemand_category/a \
-\ \ \ \ <string name=\"title_function_help\">Function documentation</string>"
+grep -RIl "\<string name=\"title_ondemand_default" res | xargs sed -i -e '/title_ondemand_default/d'
+grep -RIl "\<string name=\"title_disable_ondemand" res | xargs sed -i -e "/title_disable_ondemand/a \
+\ \ \ \ <string name=\"title_ondemand_default\">Timeout will:</string>"
 
 #grep -RIl "\<string name=\"settings_dangerous" res | xargs sed -i -e '/settings_dangerous/d'
 #grep -RIl "\<string name=\"title_check_whitelist" res | xargs sed -i -e 's/Check to allow/Check to allow:/g'
