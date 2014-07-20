067fb1dc2a28b9b38f69e173174e95de9bc47b84
M66B <marcel@faircode.eu>
2014-07-20 10:28:29 +0200

    Warning for unknown binder transaction codes

--- src/biz/bokhorst/xprivacy/XBinder.java
+++ src/biz/bokhorst/xprivacy/XBinder.java
@@ -297,7 +297,7 @@ public class XBinder extends XHook {
 			}
 			if (codeName == null) {
 				codeName = Integer.toString(code);
-				Util.log(this, Log.INFO,
+				Util.log(this, Log.WARN,
 						"Unknown transaction=" + descriptor + ":" + code + " uid=" + Binder.getCallingUid());
 				Util.logStack(this, Log.INFO);
 			}
