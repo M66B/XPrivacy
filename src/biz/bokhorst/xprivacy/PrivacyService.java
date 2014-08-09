640efea9870468aa9fc8a34fec5a4f40885583a6
M66B <marcel@faircode.eu>
2014-08-09 08:04:13 +0200

    2.99.12 beta

--- src/biz/bokhorst/xprivacy/PrivacyService.java
+++ src/biz/bokhorst/xprivacy/PrivacyService.java
@@ -77,7 +77,7 @@ public class PrivacyService extends IPrivacyService.Stub {
 	private static final String cTableUsage = "usage";
 	private static final String cTableSetting = "setting";
 
-	private static final int cCurrentVersion = 378;
+	private static final int cCurrentVersion = 379;
 	private static final String cServiceName = "xprivacy378";
 
 	private SQLiteDatabase mDb = null;
