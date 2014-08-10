634c44dc410d015b03b874626e19e622c69880c5
M66B <marcel@faircode.eu>
2014-08-10 11:20:33 +0200

    Updated text

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -2,7 +2,7 @@
 grep -RIl "\<string name=\"title_update_legacy" res | xargs sed -i -e '/title_update_legacy/d'
 
 grep -RIl "\<string name=\"title_update_none" res | xargs sed -i -e '/title_update_none/a \
-\ \ \ \ <string name=\"title_update_legacy\">XPrivacy 2 is not supported anymore, please consider to update to XPrivacy 3</string>'
+\ \ \ \ <string name=\"title_update_legacy\">XPrivacy 2 is not supported anymore, please consider upgrading to XPrivacy 3</string>'
 
 #grep -RIl "\<string name=\"app_notxposed" res | xargs sed -i -e '/app_notxposed/d'
 #grep -RIl "\<string name=\"restrict_help_internet" res | xargs sed -i -e 's/internet</Internet</g'
