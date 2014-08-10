d4a8cf736ac493317485b602370b6b16da7bf81e
M66B <marcel@faircode.eu>
2014-08-10 10:56:57 +0200

    Added legacy warning

--- src/biz/bokhorst/xprivacy/PrivacyManager.java
+++ src/biz/bokhorst/xprivacy/PrivacyManager.java
@@ -131,6 +131,7 @@ public class PrivacyManager {
 	public final static String cSettingSafeMode = "SafeMode";
 	public final static String cSettingTestVersions = "TestVersions";
 	public final static String cSettingOnDemandSystem = "OnDemandSystem";
+	public final static String cSettingLegacy = "Legacy";
 	public final static String cSettingODExpert = "ODExpert";
 	public final static String cSettingODCategory = "ODCategory";
 	public final static String cSettingODOnce = "ODOnce";
--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-grep -RIl "\<string name=\"title_update_checking" res | xargs sed -i -e '/title_update_checking/d'
-grep -RIl "\<string name=\"title_update_downloading" res | xargs sed -i -e '/title_update_downloading/d'
-grep -RIl "\<string name=\"title_update_install" res | xargs sed -i -e '/title_update_install/d'
-grep -RIl "\<string name=\"title_update_none" res | xargs sed -i -e '/title_update_none/d'
+grep -RIl "\<string name=\"title_update_legacy" res | xargs sed -i -e '/title_update_legacy/d'
 
-grep -RIl "\<string name=\"title_unsafe" res | xargs sed -i -e '/title_unsafe/a \
-\ \ \ \ <string name=\"title_update_checking\">Checking for update</string>\
-\ \ \ \ <string name=\"title_update_downloading\">Downloading update</string>\
-\ \ \ \ <string name=\"title_update_install\">Tap to install update</string>\
-\ \ \ \ <string name=\"title_update_none\">No updates available</string>'
+grep -RIl "\<string name=\"title_update_none" res | xargs sed -i -e '/title_update_none/a \
+\ \ \ \ <string name=\"title_update_legacy\">XPrivacy 2 is not supported anymore, please consider to update to XPrivacy 3</string>'
 
 #grep -RIl "\<string name=\"app_notxposed" res | xargs sed -i -e '/app_notxposed/d'
 #grep -RIl "\<string name=\"restrict_help_internet" res | xargs sed -i -e 's/internet</Internet</g'
