bb0a5ab0b79627031046389bcc341a129422ad90
M66B <marcel@faircode.eu>
2014-08-17 09:10:10 +0200

    Enabled support for Paranoid Android

--- src/biz/bokhorst/xprivacy/Hook.java
+++ src/biz/bokhorst/xprivacy/Hook.java
@@ -152,7 +152,8 @@ public class Hook implements Comparable<Hook> {
 				return true;
 			if (Build.DISPLAY == null || Build.HOST == null)
 				return false;
-			return (isAOSP() || isCyanogenMod() || isOmni() || isMIUI() || isSlim() || isCarbon() || isDirtyUnicorns() || isLiquidSmooth());
+			return (isAOSP() || isCyanogenMod() || isOmni() || isMIUI() || isSlim() || isParanoidAndroid()
+					|| isCarbon() || isDirtyUnicorns() || isLiquidSmooth());
 		} else
 			return false;
 	}
