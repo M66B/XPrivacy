3e7d53f20e454e1160d8bdad3f98d12b7e181672
M66B <marcel@faircode.eu>
2014-08-21 08:07:04 +0200

    Added help texts

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-grep -RIl "\<string name=\"settings_aosp" res | xargs sed -i -e '/settings_aosp/d'
+grep -RIl "\<string name=\"help_check_restrict" res | xargs sed -i -e '/help_check_restrict/d'
+grep -RIl "\<string name=\"help_check_ondemand" res | xargs sed -i -e '/help_check_ondemand/d'
+grep -RIl "\<string name=\"help_switch" res | xargs sed -i -e '/help_switch/d'
 
-grep -RIl "\<string name=\"settings_quirks" res | xargs sed -i -e '/settings_quirks/a \
-\ \ \ \ <string name=\"settings_aosp\">AOSP mode (requires restart)</string>'
-
-grep -RIl "\<string name=\"settings_aosp" res | xargs sed -i -e 's/requires restart/requires reboot/g'
+grep -RIl "\<string name=\"settings_aosp" res | xargs sed -i -e '/settings_aosp/a \
+\ \ \ \ <string name=\"help_check_restrict\">First check box: restrict category or function</string>\
+\ \ \ \ <string name=\"help_check_ondemand\">Second check box: restrict on demand</string>\
+\ \ \ \ <string name=\"help_switch\">On/off switch: enable or disable all restrictions</string>'
 
 #grep -RIl "\<string name=\"app_notxposed" res | xargs sed -i -e '/app_notxposed/d'
 #grep -RIl "\<string name=\"restrict_help_internet" res | xargs sed -i -e 's/internet</Internet</g'
+#grep -RIl "\<string name=\"settings_aosp" res | xargs sed -i -e 's/requires restart/requires reboot/g'
