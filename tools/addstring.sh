492f0bf08e017946fbe8791c529fe205fb3bc533
M66B <marcel@faircode.eu>
2014-08-25 09:04:20 +0200

    Added changelog menu

--- tools/addstring.sh
+++ tools/addstring.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-grep -RIl "\<string name=\"title_template_merge_reset" res | xargs sed -i -e '/title_template_merge_reset/d'
+grep -RIl "\<string name=\"menu_changelog" res | xargs sed -i -e '/menu_changelog/d'
 
-grep -RIl "\<string name=\"title_template_merge" res | xargs sed -i -e '/title_template_merge/a \
-\ \ \ \ <string name=\"title_template_merge_reset\">Apply template (merge reset)</string>'
+grep -RIl "\<string name=\"menu_tutorial" res | xargs sed -i -e '/menu_tutorial/a \
+\ \ \ \ <string name=\"menu_changelog\">Changelog</string>'
 
-grep -RIl "\<string name=\"title_template_merge" res | xargs sed -i -e 's/Apply template (merge)/Apply template (merge set)/g'
+#grep -RIl "\<string name=\"title_template_merge" res | xargs sed -i -e 's/Apply template (merge)/Apply template (merge set)/g'
 
 #grep -RIl "\<string name=\"app_notxposed" res | xargs sed -i -e '/app_notxposed/d'
 #grep -RIl "\<string name=\"restrict_help_internet" res | xargs sed -i -e 's/internet</Internet</g'
