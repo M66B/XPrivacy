1b10381f82ef1053e9abf1b3c86b1f81da69f2c9
tonymanou <tonymanou@gmail.com>
2014-09-23 11:12:23 +0200

    Fixed dark card background

--- src/biz/bokhorst/xprivacy/ActivityMain.java
+++ src/biz/bokhorst/xprivacy/ActivityMain.java
@@ -37,7 +37,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Process;
-import android.support.v7.widget.CardView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -191,12 +190,6 @@ public class ActivityMain extends ActivityBase implements OnItemSelectedListener
 
 		// Annotate
 		Meta.annotate(this.getResources());
-
-		// Hack
-		if (isDarkTheme()) {
-			CardView card = (CardView) findViewById(R.id.cvHeader);
-			card.setBackgroundColor(Color.parseColor("#30404040"));
-		}
 
 		// Get localized restriction name
 		List<String> listRestrictionName = new ArrayList<String>(PrivacyManager.getRestrictions(this).navigableKeySet());
