b993667c984eb12cf7a362c66a9b582ff40749dc
M66B <marcel@faircode.eu>
2014-10-26 08:05:21 +0100

    Revert "Fixed double tap search"
    This reverts commit de2505dfcdcd52472c07d94ce16d33021e8d61b0.
    
    Conflicts:
    	CHANGELOG.md
    
    Refs #2043

--- src/biz/bokhorst/xprivacy/ActivityMain.java
+++ src/biz/bokhorst/xprivacy/ActivityMain.java
@@ -38,7 +38,6 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Process;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
@@ -72,6 +71,7 @@ import android.widget.ProgressBar;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -490,6 +490,7 @@ public class ActivityMain extends ActivityBase implements OnItemSelectedListener
 			SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.menu_search));
 			if (searchView != null) {
 				searchView.setIconifiedByDefault(false);
+
 				searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 					@Override
 					public boolean onQueryTextChange(String newText) {
